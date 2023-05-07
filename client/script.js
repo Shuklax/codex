962919fb6620a328578129c495bc26b3c78ee725
Shuklax <harshshuklaknp@gmail.com>
2023-05-07 09:31:50 +0530

    update the url

--- client/script.js
+++ client/script.js
@@ -88,7 +88,7 @@ const handleSubmit = async (e) =>
   loader(messageDiv);
   //fetch data from the server -> he bot's response
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-fltz.onrender.comcls', {
     method: 'POST', 
     headers:{'Content-Type':'application/json'},
     body: JSON.stringify({
